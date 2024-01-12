0e2292e54ffd7c334a9baa218db95e27be5ee4fc
Dhyey <dhyeybaldha90@gmail.com>
2024-01-12 17:51:55 +0530

    string and it's method

---
+++ day_2_practical_2.java
@@ -0,0 +1,16 @@
+import java.util.*;
+
+public class day_2_practical_2 {
+    public static void main(String[] args) {
+       // String s;
+        //Scanner sc = new Scanner(System.in);
+        //s=sc.nextLine();
+        //System.out.println(s);
+        String s1 = "hello world";
+        String s2 = "hello java";
+        System.out.println(s1.charAt(2));
+        System.out.println(s1.length());
+        System.out.println(s1.compareTo(s2));
+        System.out.println(s1.concat(s2));
+    }
+}
