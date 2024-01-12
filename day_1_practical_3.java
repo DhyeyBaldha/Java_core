8826b613b1411d7166304d4413a3b9847a2bf004
Dhyey <dhyeybaldha90@gmail.com>
2024-01-12 17:04:55 +0530

    linkedlist whitout collection class

--- day_1_practical_3.java
+++ day_1_practical_3.java
@@ -10,33 +10,64 @@ public class day_1_practical_3 {
             this.next = null;
         }
     }
-    class linkedlist{
+
+    static class linkedlist {
         node head;
         node tail;
 
-        public void insert(int data){
-            node newnode= new node(data);
-            if (head==null){
-                head=newnode;
-                tail=newnode;
-            }
-            else {
-                tail.next=newnode;
-                tail=newnode;
+        public void insert(int data) {
+            node newnode = new node(data);
+            if (head == null) {
+                head = newnode;
+                tail = newnode;
+            } else {
+                tail.next = newnode;
+                tail = newnode;
             }
         }
-        public void delete(int data){
-            node current=head;
-            node privious=null;
-            while (current==null){
 
+        public void delete(int data) {
+            node current = head;
+            node previous = null;
+            while (current != null) {
+                if (current.data == data) {
+                    if (previous == null) {
+                        head = current.next;
+                    } else {
+                        previous.next = current.next;
+                    }
+                    break;
+                }
+                previous = current;
+                current = current.next;
             }
+        }
+
+        public void traverse() {
+            node current = head;
+            while (current != null) {
+                System.out.print(current.data + " ");
+                current = current.next;
+            }
+            System.out.println();
         }
     }
 
-    public static void main(String [] args){
+        public static void main(String[] args) {
+            linkedlist list = new linkedlist();
+            list.insert(1);
+            list.insert(2);
+            list.insert(3);
+            list.insert(4);
+            list.insert(5);
+            System.out.println("Original List:");
+            list.traverse();
+            list.delete(3);
+            System.out.println("List after deletion:");
+            list.traverse();
 
 
-    }
+        }
+
 
 }
